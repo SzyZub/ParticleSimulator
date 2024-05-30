d9f324aa83500b321f9a5b02ac459911d18fa9f5
Szymon Żuber <tornaos21@gmail.com>
2024-05-30 23:03:16 +0200

    finished water/sand/gravel/air

--- ParticleSimulator/ParticleSimulator.cpp
+++ ParticleSimulator/ParticleSimulator.cpp
@@ -11,8 +11,9 @@
 
 typedef enum CellType {
     air = 0,
-    sand,
-    water
+    gravel,
+    water,
+    sand
 }CellType;
 
 class MainClass {
@@ -49,18 +50,22 @@ public:
                 case air:
                     DrawRectangle(i * CELLSIDE, j * CELLSIDE, CELLSIDE, CELLSIDE, SKYBLUE);
                     break;
-                case sand:
-                    DrawRectangle(i * CELLSIDE, j * CELLSIDE, CELLSIDE, CELLSIDE, SANDCOL);
+                case gravel:
+                    DrawRectangle(i * CELLSIDE, j * CELLSIDE, CELLSIDE, CELLSIDE, GRAY);
                     break;
                 case water:
                     DrawRectangle(i * CELLSIDE, j * CELLSIDE, CELLSIDE, CELLSIDE, DARKBLUE);
+                    break;
+                case sand:
+                    DrawRectangle(i * CELLSIDE, j * CELLSIDE, CELLSIDE, CELLSIDE, SANDCOL);
                     break;
                 }
             }
         }
         DrawRectangle(10, 713, TYPESIDE, TYPESIDE, SKYBLUE);
-        DrawRectangle(70, 713, TYPESIDE, TYPESIDE, SANDCOL);
+        DrawRectangle(70, 713, TYPESIDE, TYPESIDE, GRAY);
         DrawRectangle(130, 713, TYPESIDE, TYPESIDE, DARKBLUE);
+        DrawRectangle(190, 713, TYPESIDE, TYPESIDE, SANDCOL);
         DrawText(TextFormat("%d", brushSize), 890, 725, 30, WHITE);
         DrawRectangle(930, 733, 30, 10, WHITE);
         DrawRectangle(940, 723, 10, 30, WHITE);
@@ -76,10 +81,13 @@ public:
                     selectedBrush = air;
                 }
                 else if (x > 70 && x < 70 + TYPESIDE) {
-                    selectedBrush = sand;
+                    selectedBrush = gravel;
                 }
                 else if (x > 130 && x < 130 + TYPESIDE) {
                     selectedBrush = water;
+                }
+                else if (x > 190 && x < 190 + TYPESIDE) {
+                    selectedBrush = sand;
                 }
                 else if (brushSize < 20 && x > 930 && x < 960 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                     brushSize++;
@@ -108,15 +116,15 @@ public:
                 switch (grid[i][j]) {
                 case air:
                     break;
-                case sand:
-                    if (j + 1 < GRIDH && grid[i][j + 1] != sand) {
+                case gravel:
+                    if (j + 1 < GRIDH && grid[i][j + 1] != gravel && grid[i][j + 1] != sand) {
                         grid[i][j] = grid[i][j + 1];
-                        grid[i][j + 1] = sand;
+                        grid[i][j + 1] = gravel;
                     }
                     break;
                 case water:
                     if (j + 1 < GRIDH) {
-                        if (grid[i][j + 1] != sand && grid[i][j + 1] != water) {
+                        if (grid[i][j + 1] == air) {
                             grid[i][j] = grid[i][j + 1];
                             grid[i][j + 1] = water;
                         }
@@ -124,13 +132,13 @@ public:
                             max = GRIDW;
                             min = -1;
                             for (int x = i + 1; x < GRIDW; x++) {
-                                if (grid[x][j] == sand || grid[x][j] == water) {
+                                if (grid[x][j] != air) {
                                     max = x + 1;
                                     break;
                                 }
                             }
                             for (int x = i - 1; x >= 0; x--) {
-                                if (grid[x][j] == sand || grid[x][j] == water) {
+                                if (grid[x][j] != air) {
                                     min = x - 1;
                                     break;
                                 }
@@ -162,6 +170,19 @@ public:
                             }
                         }
                     }                   
+                    break;
+                case sand:
+                    if (j + 1 < GRIDH && grid[i][j + 1] != sand && grid[i][j + 1] != gravel) {
+                        grid[i][j] = grid[i][j + 1];
+                        grid[i][j + 1] = sand;
+                    }
+                    else if (i + 1 < GRIDW && j + 1 < GRIDH && (grid[i + 1][j + 1] == air || grid[i + 1][j + 1] == water)) {
+                        grid[i][j] = grid[i + 1][j + 1];
+                        grid[i + 1][j + 1] = sand;
+                    } else if (i - 1 >= 0 && j + 1 < GRIDH && (grid[i - 1][j + 1] == air || grid[i - 1][j + 1] == water)) {
+                        grid[i][j] = grid[i - 1][j + 1];
+                        grid[i - 1][j + 1] = sand;
+                    }
                     break;
                 }
             }
